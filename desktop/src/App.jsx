97bab6a4e465a9ae42a67191edaeeb45a6706584
Muhammad Anas <anas.dev122@gmail.com>
2026-09-10 15:59:37 +0400

    configured tauri react desktop app

---
+++ desktop/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-amber-500 flex items-center justify-center">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
